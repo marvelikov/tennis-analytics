c126202decc7648aacd9e42170d28fdd7aa8c9e3
Stephane Caron <ste.caron@icloud.com>
2017-06-17 14:52:55 -0400

    Create a README file

--- Code1.R
+++ Code1.R
@@ -1,22 +1,6 @@
-# Okay guys - basic stuff
-
-# Mon frere m'a demandé de lui permettre la chose suivante :
-# Il aimerait être capable d'avoir la réponse à la question "Qui a été classé top R avant l'âge A?"
-
-# En gros, je me suis dit que c'était simplement faire un subset unique(data[age < A, rank <= R]$name)
-
-# (En fait, il voudrait un ranking par meilleur ranking parmi ceux qui répondent au critère...
-# donc parmi ceux qui répondent au critère on prend leurs meilleurs année seulement et on fait le
-# ranking final avec tout ça. Bref vous voyez le style)
-
-# Ultimement je pense qu'on peut se partir une page web et mettre de la pub google là-dessus...
-# Une page beeeen basic de stats de tennis pour commencer quoi? Quand on est prêt on regarde comment
-# ça fonctionne avec le serveur et les pubs google ou autre... Anyways là je dérape.
-
-
 # Here it goes
 
-# Le data -- sur mon ordi, je devrais le push sur le repository (J'ai pas juste 2010, mais commençons avec ça)
+# Le data -- sur mon ordi, je devrais le push sur le repository (J'ai pas juste 2010, mais commen?ons avec ?a)
 library(readr)
 Players <- read_csv("C:/Users/Samuel/Dropbox/Projects/Tennis-Fil/Players.txt", col_names = FALSE)
 T2010 <- read_csv("C:/Users/Samuel/Dropbox/Projects/Tennis-Fil/T2010.txt", col_names = FALSE)
@@ -24,7 +8,7 @@ T2010 <- read_csv("C:/Users/Samuel/Dropbox/Projects/Tennis-Fil/T2010.txt", col_n
 colnames(Players) <- c("id", "first_name", "family_name", "hand", "birth_date", "country")
 colnames(T2010) <- c("ranking_date", "rank", "id", "points")
 
-# Un peu de cleaning pour les birth_date -- pas élégant mais bon... J'ai mieux à faire
+# Un peu de cleaning pour les birth_date -- pas ?l?gant mais bon... J'ai mieux ? faire
 Players$birth_date <- sapply(Players$birth_date, function(d){
   #print(d)
   if(is.na(d)){return(NA)}
@@ -47,7 +31,7 @@ Players$birth_date <- sapply(Players$birth_date, function(d){
 })
 
 
-# Même principe pour les ranking_date
+# M?me principe pour les ranking_date
 T2010$ranking_date <- sapply(T2010$ranking_date, function(d){
   #print(d)
   if(is.na(d)){return(NA)}
@@ -82,24 +66,24 @@ T2010$ranking_date <- sapply(T2010$ranking_date, function(d){
 #})
 
 
-# Faque  le data c'est juste ça
+# Faque  le data c'est juste ?a
 data <- merge(Players, T2010)
 
-# Transforme ça en data.table
+# Transforme ?a en data.table
 library(data.table)
 data <- data.table(data)
 data
 
 
-# On peut facilement calculer l'âge en jours....
+# On peut facilement calculer l'?ge en jours....
 data$days <- as.Date(data$ranking_date) - as.Date(data$birth_date)
 
 
-# Là, jusqu'ici tout va bien
+# L?, jusqu'ici tout va bien
 data
 
 
-# Mais ça look plus avec YYYY years, MM months and DD days ....
+# Mais ?a look plus avec YYYY years, MM months and DD days ....
 rd <- as.Date(data$ranking_date)
 bd <- as.Date(data$birth_date)
 
@@ -109,13 +93,13 @@ per <- as.period(int)
 
 data[,age := per]
 
-# Pourquoi maintenant ça bug?
+# Pourquoi maintenant ?a bug?
 data
 
 
 
 
-# Aussi, les 0H 0M 0S sont fatiguants, je suis rendu là... (Ça ça marche mais pas juste le call "data")
+# Aussi, les 0H 0M 0S sont fatiguants, je suis rendu l?... (?a ?a marche mais pas juste le call "data")
 data$age[1:5]
 
 
