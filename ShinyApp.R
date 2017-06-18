478fedb5b74d7e97f7cab9feea764664745071e1
Stephane Caron <ste.caron@icloud.com>
2017-06-17 21:54:26 -0400

    Add na.rm in function

--- ShinyApp.R
+++ ShinyApp.R
@@ -21,11 +21,11 @@ data_ranking$ranking_date <- as.Date(data_ranking$ranking_date)
 filtering <- function(age_input, ranking_input, stat_input){
   data_filtered <- copy(data_ranking)
   if (stat_input == "entry"){
-    data_filtered <- data_filtered[age <= age_input & rank <= ranking_input, .(ranking_date = min(ranking_date)), id]
+    data_filtered <- data_filtered[age <= age_input & rank <= ranking_input, .(ranking_date = min(ranking_date, na.rm = TRUE)), id]
   } else {
-    data_filtered <- data_filtered[age <= age_input & rank <= ranking_input, .(rank = min(rank)), .(id)]
+    data_filtered <- data_filtered[age <= age_input & rank <= ranking_input, .(rank = min(rank, na.rm = TRUE)), .(id)]
     data_filtered <- data.table(left_join(data_filtered, data_ranking, by = c("id", "rank")))
-    data_filtered <- data_filtered[, .(ranking_date = min(ranking_date)), id]
+    data_filtered <- data_filtered[, .(ranking_date = min(ranking_date, na.rm = TRUE)), id]
   }
   data_filtered <- data.table(left_join(data_filtered, data_ranking, by = c("id", "ranking_date")))
 }
