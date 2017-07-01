49b78c59fb213454c58b8e7f6aa592a9e54d6a1d
samperochkin <samuel.perreault.3@ulaval.ca>
2017-07-01 12:45:16 -0400

    Setting targets and proposing methods.

--- MR-PreliminaryAnalysis.R
+++ MR-PreliminaryAnalysis.R
@@ -1,6 +1,18 @@
 # Illustrating our 'Match Results and Betting Odds'data
 
+# Here, I suggest we put ideas about many of the aspects of the project. Je vais ecrire en francais pas d'accents.
 
+# Il y a deux gros milestones au projet :
+
+# 1. le premier est la prediction du resultat d'un match;
+# 2. le deuxieme le resultat d'un tournoi (l'arbre du tournoi et non seulement le vaiqueur)
+
+# Toutefois, on doit se concentrer avant tout sur la data qu'on a.
+# J'ai enregistre les tournois de 2016 sur le site que Phil a partage.
+
+# J'ai commence par essayer de reformer un arbre de tournoi, pour le plaisir.
+# La tache la plus pressante est toutefois de definir un modele naif pour la prediction d'un seul match.
+# Je crois qu'on devrait commencer avec un modele logistique.
 
 
 
@@ -31,11 +43,55 @@ data_mr <- data.table(read_excel("C:/Users/Samuel/Dropbox/Git Projects/tennis-an
 
 # 2. Preliminary analysis -- data cleaning considerations
 
-# There are some warnings concerning the presence of 'N/A' values
-# They are found at [c(196,204,832,1387,1677,1696,1926),c(13,15)] and [1687,c(12,14)]
-data_mr[c(196,204,832,1387,1677,1696,1926,1687),]
+# There are some warnings concerning the presence of 'N/A' values. Most, if not all, missing values are in the
+# columns 'W3', 'L3', ...
+# This is simply because the match did not require these sets to be played...
 
 # All-in-all, the data is pretty clean... We can either try to fill the NA's
+
+
+
+
+
+# 3. Recovering a tournament tree
+
+# Say we are interested in the Rogers Masters, so we subset the data
+data_rogers <- data_mr[Tournament == "Rogers Masters"]
+
+# Let us build it top to bottom
+# First, how many rounds are there?
+tree_height <- length(unique(data_rogers$Round))
+
+# The final game...
+final_game <- data_rogers[Round == "The Final"]
+
+
+
+
+
+
+
+
+
+
+# 4. Focus on a single match outcome
+
+# The most obvious thing to start with is a basic logistic model
+
+
+
+
+
+
+
+
+
+
+# 5. Focus on a tournament tree outcome
+
+# I think the simplest thing to begin with in this case is to use our results obtained with the first model. 
+# We can artificially generate many tournament trees using the probabilities returned by the logistics and
+# create a forest of tournament trees.
 
 
 
