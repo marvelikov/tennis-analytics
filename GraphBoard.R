31cacaf69c7d95805dbd2d62a274f0cd8fe8c46d
Stephane Caron <ste.caron@icloud.com>
2017-07-10 19:26:38 -0400

    Methodology

--- GraphBoard.R
+++ GraphBoard.R
@@ -13,3 +13,12 @@ require(readxl)
 
 data <- read_xlsx("Data/MR-BO-2016.xlsx")
 
+# Methodology:
+# We have to start by making a function that will sort our data in an appropriate order before
+# filling our graph. Why ? It's because our tournament tree is determined in advance so we 
+# have to place the matchs up in each round in the correct order. 
+
+# This part is a little tricky because the order of a given round will be determined by the
+# data of the next round. In fact, the match up of the next round will tell us which match up
+# have to be placed back to back.
+
