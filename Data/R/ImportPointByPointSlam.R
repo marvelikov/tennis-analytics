cdabc7b11f7d547a36a71c23eb13257d4225f0f2
Stephane Caron <ste.caron@icloud.com>
2017-08-26 09:07:39 -0400

    Separate import script

--- Data/R/ImportPointByPointSlam.R
+++ Data/R/ImportPointByPointSlam.R
@@ -1,6 +1,7 @@
-# Date: 5 Aout 2017
+# Title: ImportPointByPointSlam
+# Date: 26 Aout 2017
 # Author: Stephane Caron  
-# Subject: Extract ATP game data from GitHub JackSackmann
+# Subject: Extract ATP point by point data from JackSackmann's GitHub repo
 
 # 0. Load packages --------------------------------------------------------
 
@@ -10,26 +11,7 @@ library(data.table)
 library(readr)
 
 
-# 1. Import data ----------------------------------------------------------
-
-# Define a function to import data from given year to another by using data stored in JackSackmann GitHub repo.
-
-import_games <- function(year_from, year_to){
-  i <- 1
-  data_x <- list(NA)
-  for (year in (year_from:year_to)){
-    x <- getURL(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_matches_", year, ".csv"))
-    data_x[[i]] <- read.csv(textConnection(x))
-    i <-i + 1
-  }
-  data_games <- do.call(rbind, data_x)
-}
-
-data <- import_games(year_from = 2010, year_to = 2017)
-
-
-
-# 2. Point by point data --------------------------------------------------
+# 1. Extract point by point data from grand slam  --------------------------------------------------
 
 tournement <- c("ausopen", "frenchopen", "wimbledon", "usopen")
 year <- seq(2011, 2016, 1)
@@ -49,8 +31,10 @@ import_point_by_point <- function(x){
 }
 
 data_pts_by_pts <- import_point_by_point(x = grand_slam)
-
 save(data_pts_by_pts, file = "Data/Raw/pts_by_pts.RData")
+
+
+# 2. Import grand slam games info -----------------------------------------
 
 import_matches <- function(x){
   i <- 1
@@ -63,8 +47,7 @@ import_matches <- function(x){
   data_point_by_point <- rbindlist(data_x)
 }
 
-data_matches <- import_point_by_point(x = grand_slam[2])
+data_matches <- import_point_by_point(x = grand_slam)
+save(data_matches, file = "Data/Raw/grand_slam_matches.RData")
 
 
-import <- getURL(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_slam_pointbypoint/master/", grand_slam[1]))
-test <- read.csv(textConnection(import))
