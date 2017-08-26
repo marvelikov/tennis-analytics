3bd95188d692f0966084fd89b0ea32f424b61b8b
Stephane Caron <ste.caron@icloud.com>
2017-08-26 00:33:01 -0400

    Save 2012 data

--- Data/R/ImportData.R
+++ Data/R/ImportData.R
@@ -6,6 +6,7 @@
 
 library(tidyverse)
 library(RCurl)
+library(data.table)
 library(readr)
 
 
@@ -25,3 +26,45 @@ import_games <- function(year_from, year_to){
 }
 
 data <- import_games(year_from = 2010, year_to = 2017)
+
+
+
+# 2. Point by point data --------------------------------------------------
+
+tournement <- c("ausopen", "frenchopen", "wimbledon", "usopen")
+year <- seq(2012, 2012, 1)
+
+grid <- expand.grid(year, tournement)
+grand_slam <- c(sapply(1:nrow(grid), function(i) {paste0(grid$Var1[i], "-", grid$Var2[i])}))
+
+import_point_by_point <- function(x){
+  i <- 1
+  data_x <- list(NA)
+  for (link in x){
+    import <- getURL(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_slam_pointbypoint/master/", link, "-points.csv"))
+    data_x[[i]] <- data.table(read.csv(textConnection(import)))
+    i <- i + 1
+  }
+  data_point_by_point <- rbindlist(data_x)
+}
+
+data_pts_by_pts <- import_point_by_point(x = grand_slam)
+
+save(data_pts_by_pts, file = "Data/Raw/2012_pts_by_pts.RData")
+
+import_matches <- function(x){
+  i <- 1
+  data_x <- list(NA)
+  for (link in x){
+    import <- getURL(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_slam_pointbypoint/master/", link, "-matches.csv"))
+    data_x[[i]] <- data.table(read.csv(textConnection(import)))
+    i <-i + 1
+  }
+  data_point_by_point <- rbindlist(data_x)
+}
+
+data_matches <- import_point_by_point(x = grand_slam[2])
+
+
+import <- getURL(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_slam_pointbypoint/master/", grand_slam[1]))
+test <- read.csv(textConnection(import))
