af28fed3e7d82fc27bd5809d9e9d05564a868027
Stephane Caron <ste.caron@icloud.com>
2017-09-05 07:03:10 -0400

    Test for correlations between variables

--- Modeling/Logistic/logistic modeling.R
+++ Modeling/Logistic/logistic modeling.R
@@ -7,23 +7,32 @@ require(lubridate)
 require(data.table)
 require(readr)
 require(readxl)
+require(corrplot)
 
 # 1. Import data ----------------------------------------------------------
 
-load("Data/Raw/pts_by_pts.RData")
-data <- data_pts_by_pts
-rm("data_pts_by_pts")
-summary(data)
-
-load("Data/Raw/data_slam_matches.RData")
-data.matches <- data_slam_matches
-rm("data_slam_matches")
-summary(data.matches)
+source("Modeling/Logistic/DataPreparation.R")
 
 
+# 2. Look at correlations -------------------------------------------------
+
+# Faudrait revoir si ces transformations sont tous appropriées (surtout lorsque j'enlève les NAs ... faudrait peut etre plus fouillé sur pourquoi les valeurs sont vides)
+test_modeling <- data_modeling[,-c(1:2)]
+test_modeling$p1_score <- as.numeric(test_modeling$p1_score)
+test_modeling$p2_score <- as.numeric(test_modeling$p2_score)
+test_modeling <- test_modeling[-which(is.na(test_modeling$set_no)),]
+test_modeling[which(is.na(p1_bp_perc)),] <- 0
+test_modeling[which(is.na(p2_bp_perc)),] <- 0
+test_modeling <- test_modeling[-which(is.na(p1_cum_ace)),]
 
 
-test <- data[P1_won_point == 0 & P2_won_point == 0,]
+str(test_modeling)
+summary(test_modeling)
 
+M <- cor(test_modeling)
+corrplot(M)
+
+
+# 3.  ---------------------------------------------------------------------
 
 
