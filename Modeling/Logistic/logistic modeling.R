b8a07116d282c33b063f7a98c62fb1bcdbed90c5
Stephane Caron <ste.caron@icloud.com>
2017-09-05 07:29:46 -0400

    Split training/test data

--- Modeling/Logistic/logistic modeling.R
+++ Modeling/Logistic/logistic modeling.R
@@ -18,7 +18,7 @@ source("Modeling/Logistic/DataPreparation.R")
 # 2. Look at correlations -------------------------------------------------
 
 # Faudrait revoir si ces transformations sont tous appropriées (surtout lorsque j'enlève les NAs ... faudrait peut etre plus fouillé sur pourquoi les valeurs sont vides)
-test_modeling <- data_modeling[,-c(1:2)]
+test_modeling <- data_modeling[, -c(1:2)]
 test_modeling$p1_score <- as.numeric(test_modeling$p1_score)
 test_modeling$p2_score <- as.numeric(test_modeling$p2_score)
 test_modeling <- test_modeling[-which(is.na(test_modeling$set_no)),]
@@ -32,7 +32,7 @@ test_modeling <- test_modeling[-which(is.na(p1_cum_ace)),]
 str(test_modeling)
 summary(test_modeling)
 
-M <- cor(test_modeling[,-c("winner")])
+M <- cor(test_modeling[, -c("winner")])
 corrplot(M)
 
 
