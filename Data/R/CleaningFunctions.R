4d3ef913bf1a9782ea83af0f5b7786dc8950a704
Stephane Caron <ste.caron@icloud.com>
2017-09-07 19:45:12 -0400

    Set up cleaning functions

---
+++ Data/R/CleaningFunctions.R
@@ -0,0 +1,9 @@
+# Title: CleaningFunctions
+# Date: 9 Septembre 2017
+# Author: Stephane Caron
+# Subject: Define functions used to clean the data
+
+sum_last_days <- function(x, nb_days) {
+  
+}
+
