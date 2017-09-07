26c33048d47070644c276d382a12ebe8e62e4a27
samperochkin <samuel.perreault.3@ulaval.ca>
2017-09-07 15:21:37 -0400

    Created aceMomentumData.Rda which contains a data.table: the result obtained with momentumCalculator.R

--- Modeling/Logistic/momentumCalculator.R
+++ Modeling/Logistic/momentumCalculator.R
@@ -1,17 +1,31 @@
-momentumCalculator <- function(vec, fun.name = ""){
+#id <- data$match_id[1]
+
+
+aceMomentum.data <- sapply(unique(data$match_id), function(id){
+  print(as.character(id))
   
-  len <- length(vec)
+  sub.data <- subset(data, match_id == id & !is.na(PointServer), c(match_id, P1Ace, P2Ace, PointServer))
   
-  if(nchar(fun.name) == 0 | fun.name == "halving"){
+  P1Serves <- which(sub.data$PointServer == 1)
+  P2Serves <- which(sub.data$PointServer == 2)
+  
+  w <- 1/2^(1:max(c(length(P1Serves),length(P2Serves))))
+  
+  aceMomentumP1 <- rep(NA, nrow(sub.data))
+  l1 <- sum(sub.data$PointServer == 1)
     
-    w <- 1/2^(0:(len-1))
+  aceMomentumP1[sub.data$PointServer == 1] <- sapply(1:l1, function(i){
+    sum(w[i:1] * sub.data[PointServer == 1,P1Ace][1:i])
+  })
+
+  aceMomentumP2 <- rep(NA, nrow(sub.data))
+  l2 <- sum(sub.data$PointServer == 2)
+  aceMomentumP2[sub.data$PointServer == 2] <- sapply(1:l2, function(i){
+    sum(w[i:1] * sub.data[PointServer == 2,P2Ace][1:i])
+  })
     
-    mom <- sapply(1:length(vec), function(i){
-      sum(w[1:i] * vec[1:i]) / sum(w[1:i])
-    })
-  }
+  data.table(match_id = rep(id, nrow(sub.data)), aceMomentumP1 = aceMomentumP1, aceMomentumP2 = aceMomentumP2)
+})
   
-  
-  
-  return(mom)
-}
+aceMomentum.data <- data.table(match_id = aceMomentum.data[1,], aceMomentumP1 = aceMomentum.data[2,], aceMomentumP2 = aceMomentum.data[3,])
+saveRDS(aceMomentum.data, file="aceMomentumData.Rda")
