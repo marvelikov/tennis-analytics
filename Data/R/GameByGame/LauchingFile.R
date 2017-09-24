fdb6212ad0a4f9a0a6e1001a3163b3b701fc3a85
Stephane Caron <ste.caron@icloud.com>
2017-09-23 20:01:35 -0400

    Set up a lauching file to update game by game data

---
+++ Data/R/GameByGame/LauchingFile.R
@@ -0,0 +1,20 @@
+# Title: LauchingFile
+# Date: 23 septembre 2017
+# Author: Stephane Caron
+# Subject: Lauching file to update game by game data
+
+# I wanna build a lauching file that will be used to update/clean/structure game by game tennis data ...
+
+# Set wanted variables ----------------------------------------------------
+
+start_year <- 2010
+end_year <- 2017
+aggregated_time_frame_days <- 365
+
+
+# Run needed scripts ------------------------------------------------------
+
+# Not completed yet ...
+source("Import")
+source("Transform")
+source("Summarise")
