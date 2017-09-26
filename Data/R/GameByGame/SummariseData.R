2c19fb1cc451999d9ec6fcab74857ab83cfeab09
Stephane Caron <ste.caron@icloud.com>
2017-09-25 20:34:57 -0400

    Define ratios that will be used as variables for modeling

--- Data/R/GameByGame/SummariseData.R
+++ Data/R/GameByGame/SummariseData.R
@@ -37,7 +37,7 @@ summarise_variable <- function(data, colname, nb_days = 365, fill_missing = NA) 
 }
 
 # Define col to summarise
-col_summarise <- c("svpt", "1stIn", "1stWon", "2ndWon", "bpFaced", "bpSaved", "rpt_won", "win", "loss")
+col_summarise <- c("svpt", "1stIn", "1stWon", "2ndWon", "bpFaced", "bpSaved", "rpt_won", "rpt", "win", "loss")
 
 # Copy the transformed data
 data_summarised <- copy(data_transformed)
@@ -47,10 +47,31 @@ sapply(col_summarise, function(x) summarise_variable(data_summarised, x, nb_days
 
 
 
-# Convert summarised data into per game ratios ----------------------------
-
-
 # Calculate tennis stats variables/ratios ---------------------------------
 
+# Ratio no.1: Winning percentage on 1st serve
+data_summarised[, perc_1st_serve_won := `1stWon_365`/svpt_365]
+
+# Ratio no.2: Winning percentage on 2nd serve
+data_summarised[, perc_2nd_serve_won := `2ndWon_365`/svpt_365]
+
+# Ratio no.3: Winning percentage on return serve 
+data_summarised[, perc_return_won := rpt_won_365/rpt_365]
+
+# Ratio no.4: Winning percentage on break point
+data_summarised[, perc_bp := bpSaved_365/bpFaced_365]
+
+# Ratio no.5: Winning percentage of match
+data_summarised[, perc_win := win_365/(win_365 + loss_365)]
+
+# Ratio no.6: Average point per game
+data_summarised[, ave_pts_game := (`1stWon_365` + `2ndWon_365` + rpt_won_365)/(win_365 + loss_365)]
+
+# Remove NaN
+data_summarised[is.na(svpt_365), perc_1st_serve_won := 0]
+data_summarised[is.na(rpt_365), perc_2nd_serve_won := 0]
+data_summarised[is.na(bpFaced_365) | bpFaced_365 == 0, perc_bp := 0]
+data_summarised[is.na(win_365 + loss_365), perc_win := 0]
+data_summarised[is.na(win_365 + loss_365), ave_pts_game := 0]
 
   
