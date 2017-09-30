51e2124beee27d37b0545383c74c0d8aca0d18fc
Stephane Caron <ste.caron@icloud.com>
2017-09-30 14:11:16 -0400

    Bring back the data to the original form (1 line for a single game)

--- Data/R/GameByGame/ModelingData.R
+++ Data/R/GameByGame/ModelingData.R
@@ -16,37 +16,38 @@ data_summarised <- fread("Data/Cleaned/DataSummarised.csv")
 
 # Transform data for modeling purposes ------------------------------------
 
-cols_modeling <- c("tourney_date", "match_num", "name", "perc_1st_serve_won", "perc_2nd_serve_won", "perc_return_won", "perc_bp", "perc_win", "ave_pts_game", "surface", "win")
-data_pre_modeling <- data_summarised[, !cols_modeling, with = FALSE]
+cols_modeling <- c("tourney_date", "match_num", "tourney_name", "name", "perc_1st_serve_won", "perc_2nd_serve_won", "perc_return_won", "perc_bp", "perc_win", "ave_pts_game", "surface", "win")
+data_pre_modeling <- data_summarised[, cols_modeling, with = FALSE]
 
-data_summarised_winner <- data_summarised[win == 1,]
-new_names_win <- sapply(1:names(data_summarised_winner), function(x) {
-  if (x != c("tourney_date", "match_num", "surface")) {
-    paste0("w_", x)
+data_pre_modeling_winner <- data_pre_modeling[win == 1,]
+new_names_win <- sapply(1:length(data_pre_modeling_winner), function(x) {
+  if (names(data_pre_modeling_winner)[x] %in% c("tourney_date", "tourney_name" , "match_num", "surface")) {
+    names(data_pre_modeling_winner)[x]
   } else {
-    x
+    names(data_pre_modeling_winner)[x] <- paste0("w_", names(data_pre_modeling_winner)[x])
   }
 })
-names(data_summarised_winner) <- new_names_win
+names(data_pre_modeling_winner) <- new_names_win
 
-data_summarised_looser <- data_summarised[win == 0,]
-new_names_loss <- sapply(1:names(data_summarised_winner), function(x) {
-  if (x != c("tourney_date", "surface")) {
-    paste0("l_", x)
+data_pre_modeling_looser <- data_pre_modeling[win == 0,]
+new_names_loss <- sapply(1:length(data_pre_modeling_looser), function(x) {
+  if (names(data_pre_modeling_looser)[x] %in% c("tourney_date", "tourney_name" , "match_num", "surface")) {
+    names(data_pre_modeling_looser)[x]
   } else {
-    x
+    names(data_pre_modeling_looser)[x] <- paste0("l_", names(data_pre_modeling_looser)[x])
   }
 })
-names(data_summarised_looser) <- new_names_loss
+names(data_pre_modeling_looser) <- new_names_loss
 
 # Rejoin both tables together
-setkey(data_summarised_winner, tourney_date, match_num, surface)
-setkey(data_summarised_looser, tourney_date, match_num, surface)
-data_summarised_winner[data_summarised_looser]
+setkey(data_pre_modeling_winner, tourney_date, tourney_name, match_num, surface)
+setkey(data_pre_modeling_looser, tourney_date, tourney_name, match_num, surface)
+data_pre_modeling <- data_pre_modeling_winner[data_pre_modeling_looser]
+rm(data_pre_modeling_winner, data_pre_modeling_looser)
 
-data_modeling <- data_summarised %>% 
+data_modeling <- data_pre_modeling %>% 
                     select(
-                      -c(w_win, l_win)
+                      -c(w_win, l_win, tourney_name)
                     ) %>% 
                     spread(
                       surface,
@@ -63,8 +64,8 @@ data_modeling <- data_summarised %>%
                       -c(Hard, Clay, Grass)
                     ) %>% 
                     rename(
-                      p1 = winner_name,
-                      p2 = loser_name
+                      p1 = w_name,
+                      p2 = l_name
                     )
 
 data_modeling <- data.table(data_modeling)
@@ -72,7 +73,6 @@ data_modeling <- data.table(data_modeling)
 new_names <- gsub("w_", "p1_", colnames(data_modeling))
 new_names <- gsub("l_", "p2_", new_names)
 colnames(data_modeling) <- new_names
-
 
 # Swap the columns
 source("Data/R/GameByGame/SwapColumns.R")
