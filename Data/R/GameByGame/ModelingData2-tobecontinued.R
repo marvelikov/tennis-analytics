73086c49a9ec5c1a05ee8a830cb5a63c9ac9043e
Stephane Caron <ste.caron@icloud.com>
2017-10-11 22:46:45 -0400

    Split grid stuff

--- Data/R/GameByGame/ModelingData2-tobecontinued.R
+++ Data/R/GameByGame/ModelingData2-tobecontinued.R
@@ -14,6 +14,7 @@ library(lubridate)
 # Load transformed data ---------------------------------------------------
 
 data_transformed <- fread("Data/Cleaned/DataTransformed.csv")
+data_transformed <- fread("Data/R/GameByGame/data_transformed2.csv")
 data_transformed$tourney_date <- ymd(data_transformed$tourney_date)
 
 # Summarise data  ---------------------------------------------------------
@@ -47,30 +48,41 @@ sapply(X = unique_tf, FUN = function(time) {
 
 lapply(X = variables_to_summarise, function(x) {
   print(x[["var"]])
-  summarised_col_name <- paste0(x[["var"]], "_", x[["time_frame"]])
   col_to_summarised <- which(names(data_transformed) == x[["var"]])
   
-  data_transformed[, (summarised_col_name) := sapply(1:nrow(data_transformed), FUN = function(i) {
-    if (length(unlist(data_transformed[i, paste0("targeted_rows_", x[["time_frame"]]), with = FALSE])) == 0) {
-      NA
-    } else {
-      if(is.null(x[[split]])){
-        sum(data_transformed[unlist(data_transformed[i, paste0("targeted_rows_", x[["time_frame"]]), with = FALSE]), x[["var"]], with = FALSE], na.rm = TRUE)
-      }else{
-        # modify so that it splits on the right variable
-        sum(data_transformed[unlist(data_transformed[i, paste0("targeted_rows_", x[["time_frame"]]), with = FALSE]), x[["var"]], with = FALSE], na.rm = TRUE)
+  if(is.null(x[["split"]])) {
+    split_grid <- cbind(data.table(var = x[["var"]]), data.table(time_frame = x[["time_frame"]]))
+  } else {
+    split_grid <- cbind(data.table(var = x[["var"]]), expand.grid(sapply(x[["split"]], function(split_var) {
+      unique(data_transformed[, split_var, with = FALSE])
+    }, USE.NAMES = FALSE)), data.table(time_frame = x[["time_frame"]])) 
+  }
+  
+  sapply(1:nrow(split_grid), function(row) {
+    data_transformed[, (paste(split_grid[row,], collapse = "_")) := sapply(1:nrow(data_transformed), FUN = function(i) {
+      if (length(unlist(data_transformed[i, paste0("targeted_rows_", x[["time_frame"]]), with = FALSE])) == 0) {
+        NA
+      } else {
+        text_condition <- paste0(sapply(x[["split"]], function(col) {
+          paste0("[", col, "=='", split_grid[row, col], "']")
+        }), collapse = "")
+        text_eval <- paste0("sum(data_transformed[unlist(data_transformed[i, paste0('targeted_rows_', x[['time_frame']]), with = FALSE]), x[['var']], with = FALSE]", text_condition, ", na.rm = TRUE)")
+        print(row)
+        eval(parse(text = text_eval))
       }
     }
-  })]
+   )]
+  })
 })
 
 # Copy the transformed data
 data_summarised <- copy(data_transformed)
 
+test <- variables_to_summarise[[9]]
 
-
-
-
+split_grid <- cbind(data.table(var = test[["var"]]), expand.grid(sapply(test[["split"]], function(split_var) {
+  unique(data_transformed[, split_var, with = FALSE])
+}, USE.NAMES = FALSE)), data.table(time_frame = test[["time_frame"]])) 
 
 
 
