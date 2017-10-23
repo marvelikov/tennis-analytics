46eec31656eee40b9aa96fd289c800b2858657b6
Stephane Caron <ste.caron@icloud.com>
2017-10-22 21:16:59 -0400

    Rename fils and kill dev scripts

--- Data/R/GameByGame/CreateVariablesData.R
+++ Data/R/GameByGame/CreateVariablesData.R
@@ -7,11 +7,12 @@ library(data.table)
 
 # Import summarised data --------------------------------------------------
 
+split_data_into <- 4
 data_summarised <- import_splitted_data(filename = "Data/Cleaned/DataSummarised", split_number = split_data_into)
 # data_summarised <- fread("Data/Cleaned/DataSummarised.csv", stringsAsFactors = FALSE)
 
 
-# Creates variables -------------------------------------------------------
+# Creates variables explicatives  -------------------------------------------------------
 
 data_summarised[, perc_1st_serve_won_30 := `1stWon_30`/svpt_30]
 data_summarised[, perc_1st_serve_won_365 := `1stWon_365`/svpt_365]
@@ -38,46 +39,24 @@ data_summarised[, top_4_GS := top_4_GS_99999]
 data_summarised[, min_played := minutes_3]
 data_summarised[, injury := retired_365/game_played_365]
 
+#######################################
+#### Adding Uncertainty indicators ####
+#######################################
+data_summarised[, game_played_30_uncertainty := 1/game_played_30]
+data_summarised[, game_played_365_uncertainty := 1/game_played_365]
+data_summarised[, game_played_clay_uncertainty := 1/game_played_clay]
+data_summarised[, game_played_grass_uncertainty := 1/game_played_grass]
+data_summarised[, game_played_hard_uncertainty := 1/game_played_hard]
 
 
-# Calculate tennis stats variables/ratios 
-# 
-# # Ratio no.1: Winning percentage on 1st serve
-# data_summarised[, perc_1st_serve_won := `1stWon_365`/svpt_365]
-# 
-# # Ratio no.2: Winning percentage on 2nd serve
-# data_summarised[, perc_2nd_serve_won := `2ndWon_365`/svpt_365]
-# 
-# # Ratio no.3: Winning percentage on return serve 
-# data_summarised[, perc_return_won := rpt_won_365/rpt_365]
-# 
-# # Ratio no.4: Winning percentage on break point
-# data_summarised[, perc_bp := bpSaved_365/bpFaced_365]
-# 
-# # Number of games played by surface in last 3 years
-# data_summarised[, game_played_clay := (win_Clay_1095 + loss_Clay_1095)]
-# data_summarised[, game_played_grass := (win_Grass_1095 + loss_Grass_1095)]
-# data_summarised[, game_played_hard := (win_Hard_1095 + loss_Hard_1095)]
-# 
-# # Ratio no.5: Winning percentage of match per surface
-# data_summarised[, perc_win_clay := win_Clay_1095/game_played_clay]
-# data_summarised[, perc_win_grass := win_Grass_1095/game_played_grass]
-# data_summarised[, perc_win_hard := win_Hard_1095/game_played_hard]
-# 
-# # Number of games last year
-# data_summarised[, game_played := (win_365 + loss_365)]
-# 
-# # Ratio no.6: Winning percentage of match all surfaces in last year
-# data_summarised[, perc_win := win_365/game_played]
-# 
-# # Ratio no.6: Average point per game
-# data_summarised[, ave_pts_game := (`1stWon_365` + `2ndWon_365` + rpt_won_365)/game_played]
-# 
-# # Ratio no.7: 1st serve In percentage
-# data_summarised[, perc_1st_in := `1stIn_365`/svpt_365]
+# Create new response variable --------------------------------------------
+
+data_summarised[, score_sum := unlist(lapply(.I, function(i) {
+  sum(data_summarised[i, c("score_set_1", "score_set_2", "score_set_3", "score_set_4", "score_set_5"), with = FALSE], na.rm = TRUE)
+}))]
 
 
-
+# Correct null ratios -----------------------------------------------------
 
 # Remove NaN
 data_summarised[svpt_365 == 0, perc_1st_serve_won_365 := 0]
@@ -97,5 +76,16 @@ data_summarised[game_played_hard == 0, perc_win_hard := 0]
 data_summarised[svpt_365 == 0, perc_1st_in_365 := 0]
 data_summarised[svpt_30 == 0, perc_1st_in_30 := 0]
 data_summarised[game_played_365 == 0, injury := 0]
+
+
+###################################################################
+#### putting 1 (extreme uncertainty) when no games were played ####
+###################################################################
+data_summarised[game_played_365 == 0, game_played_365_uncertainty := 1]
+data_summarised[game_played_30 == 0, game_played_30_uncertainty := 1]
+data_summarised[game_played_clay == 0, game_played_clay_uncertainty := 1]
+data_summarised[game_played_grass == 0, game_played_grass_uncertainty := 1]
+data_summarised[game_played_hard == 0, game_played_hard_uncertainty := 1]
+
 
 data_pre_modeling <- data_summarised
