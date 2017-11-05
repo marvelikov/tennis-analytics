a87faf384b13c291400560f652d9026c04c2b7b2
samperochkin <samuel.perreault.3@ulaval.ca>
2017-11-05 10:35:16 -0500

    Added inputs for the conditions in which the match is played.

--- Modeling/NeuralNetworks/NewStructurePreview.R
+++ Modeling/NeuralNetworks/NewStructurePreview.R
@@ -4,15 +4,22 @@
 # One reccurent net per player.
 names <- unique(names)
 
-input_layers <- list()
+players_layers <- list()
 for(i in seq_along(names)){
-  input_layers[[i]] <- layer_gru(units = 2 * nb_variables, input_shape = nb_variables) %>%
+  players_layers[[i]] <- layer_gru(units = 2 * nb_variables, input_shape = nb_variables) %>%
     layer_gru(units = 2 * nb_variables) %>%
     layer_gru(units = 2 * nb_variables) %>%
     layer_gru(units = 2 * nb_variables)
 }
 
-predictions <- layer_concatenate(unlist(input_layers, recursive = FALSE), axis=-1) %>%
+# An input layers for the conditions in which the match is played (surface, weather, etc...)
+conditions_layer <-   layer_dense(units = 10 * nb_variables, activation = "tanh", input_shape = nb_variables) %>%
+  layer_dropout(rate = 0.4)
+
+# We put all that together into one input layer for a feedforward!
+input_layers <- c(players_layers, list(conditions_layer))
+
+predictions <- layer_concatenate((input_layers, recursive = FALSE), axis=-1) %>%
   layer_dense(units = 10 * nb_variables, activation = "tanh") %>%
   layer_dropout(rate = 0.4) %>%
   layer_dense(units = 10 * nb_variables, activation = "tanh") %>%
